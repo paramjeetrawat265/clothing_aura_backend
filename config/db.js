af0f46eb56358a1505c7a9f5665e5fab2ac9893f
paramjeetrawat265 <paramjeetrawat265@gmail.com>
2022-11-13 22:25:18 +0530

    changse

--- config/db.js
+++ config/db.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 const connection = mongoose.connect(process.env.MONGO_URL)
+// helloffffffffffffffffffff
+// f
 
+// ff
+// f
+// f
+// f
+// ff
+// f
+// ff
 module.exports = {
     connection
 }
